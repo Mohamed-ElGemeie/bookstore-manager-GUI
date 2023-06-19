f600156fde46c78ea212c5c8a4d16940a02b9333
MohamedElgemeie <mgalal2002@outlook.com>
2023-06-19 13:54:41 +0300

    .

--- db/db_bookstore_relations.sql
+++
@@ -1,6 +0,0 @@
-Use db_bookstore;
-
-ALTER TABLE instore ADD FOREIGN KEY (tran_id) REFERENCES transactions(id) ON DELETE SET NULL;
-ALTER TABLE order_group ADD FOREIGN KEY (tran_id) REFERENCES transactions(id) ON DELETE SET NULL;
-ALTER TABLE order_group ADD FOREIGN KEY (item_id) REFERENCES item(id) ON DELETE SET NULL;
-ALTER TABLE print_stock ADD FOREIGN KEY (tran_id) REFERENCES transactions(id) ON DELETE SET NULL;
